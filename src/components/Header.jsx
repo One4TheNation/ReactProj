4bd6bb355b9a0407c8d82937faaaf560b603fad7
Navarre <jpnavarre@Navarres-iMac.local>
2024-10-24 10:25:30 -0400

    updated header and main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,40 +5,42 @@ function Header() {
   const currentPage = useLocation().pathname;
 
   return (
-    <header className="header header-expand-xl d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <Link
-        to="/"
-        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
-      >
-        <svg className="bi me-2" width="40" height="32">
-          <use xlink:href="#smile"></use>
-        </svg>
-        <span className="fs-4">Navarre</span>
-      </Link>
+    <container>
+      <header className="header header-expand-lg d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bg-primary">
+        <Link
+          to="/"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none text-white"
+        >
+          <svg className="bi me-2" width="40" height="32">
+            <use xlink:href="#smile"></use>
+          </svg>
+          <span className="fs-4">Navarre</span>
+        </Link>
 
-      <ul className="nav nav-pills">
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a href="#" className="nav-link">
-            Portfolio
-          </a>
-        </li>
-        <li className="nav-item">
-          <Link to="/resume" className="nav-link">
-            Resume
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a href="#" className="nav-link">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </header>
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <Link to="/about" className="nav-link text-white">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <Link to="/resume" className="nav-link text-white">
+              Resume
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
+    </container>
   );
 }
 
