458bee4617fbdd7fab1b83a3330ac26f7c55a87e
One4TheNation <166298781+One4TheNation@users.noreply.github.com>
2025-03-13 14:08:46 -0400

    fixing contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -6,7 +6,34 @@ function ContactPage(props) {
     <main
       role="main"
       className="container text-center d-flex flex-column align-items-center justify-content-center"
-    ></main>
+    >
+<div className="container my-5">
+  <div className="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5">
+    {/* <button type="button" className="position-absolute top-0 end-0 p-3 m-3 btn-close bg-secondary bg-opacity-10 rounded-pill" aria-label="Close"></button> */}
+    {/* <svg className="bi mt-5 mb-3" width="48" height="48"><use xlink:href="#check2-circle"></use></svg> */}
+    <h1 className="text-body-emphasis">Placeholder jumbotron</h1>
+    <p className="col-lg-6 mx-auto mb-4">
+      This faded back jumbotron is useful for placeholder content. It's also a great way to add a bit of context to a page or section when no content is available and to encourage visitors to take a specific action.
+    </p>
+    <button className="btn btn-primary px-5 mb-5" type="button">
+      Call to action
+    </button>
+  </div>
+</div>
+
+{/* <div className="b-example-divider"></div> */}
+
+  <h1 className="text-body-emphasis">Get started with Bootstrap</h1> 
+  <p className="fs-5 col-md-8">Quickly and easily get started with Bootstrap's compiled, production-ready files with this barebones example featuring some basic HTML and helpful links. Download all our examples to get started.</p>
+
+<div className="mb-5">
+  <a href="/docs/5.3/examples/" className="btn btn-primary btn-lg px-4">Download examples</a>
+</div>
+
+    </main>
   );
 }
+
 export default ContactPage;
+
+
